10c1ad9c775142b20294ce61a0f143ea62008a56
Batuhan Balta <bthn.balta@gmail.com>
2021-01-21 22:41:24 -0800

    data's fetched with axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React, { useEffect, useState } from "react";
+
 import axios from "axios";
 
-function App() {
-  return <div className="App"></div>;
-}
+export default function App() {
+  const [covid, setCovid] = useState([]);
 
-export default App;
+  useEffect(() => {
+    axios
+      .get("https://api.covidtracking.com/v1/us/current.json")
+      .then((response) => setCovid(response.data))
+      .catch((error) => console.log({ error }));
+  }, []);
+  console.log("zaza", covid);
+
+  {
+    return (
+      <div className="covid-19">
+        {covid.map((prop) => {
+          return (
+            <div key={prop.name}>
+              <h2>{prop.date}</h2>
+              <h2>{prop.death}</h2>
+              <h2>{prop.deathIncrease}</h2>
+              <h2>{prop.hospitalized}</h2>
+              <h2>{prop.hospitalizedCurrently}</h2>
+              <h2>{prop.hospitalizedIncrease}</h2>
+              <h2>{prop.inIcuCumulative}</h2>
+              <h2>{prop.inIcuCurrently}</h2>
+              <h2>{prop.negative}</h2>
+              <h2>{prop.negativeIncrease}</h2>
+              <h2>{prop.onVentilatorCurrently}</h2>
+              <h2>{prop.onVentilatorCumulative}</h2>
+              <h2>{prop.pending}</h2>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
