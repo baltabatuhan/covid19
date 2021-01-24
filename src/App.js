65250d95d5cafef61d8d74971c7bff7077b4acd2
Batuhan Balta <bthn.balta@gmail.com>
2021-01-23 23:52:40 -0800

    some updates

--- src/App.js
+++ src/App.js
@@ -19,21 +19,22 @@ export default function App() {
         {covid.map((prop) => {
           return (
             <div key={prop.name}>
-              <h2>Date: {prop.date}</h2>
-              <h2>Death: {prop.death}</h2>
-              <h2>{prop.deathIncrease}</h2>
-              <h2>{prop.hospitalized}</h2>
-              <h2>{prop.hospitalizedCurrently}</h2>
-              <h2>{prop.hospitalizedIncrease}</h2>
-              <h2>{prop.inIcuCumulative}</h2>
-              <h2>{prop.inIcuCurrently}</h2>
-              <h2>{prop.negative}</h2>
-              <h2>{prop.negativeIncrease}</h2>
-              <h2>{prop.positive}</h2>
-              <h2>{prop.positiveIncrease}</h2>
-              <h2>{prop.onVentilatorCurrently}</h2>
-              <h2>{prop.onVentilatorCumulative}</h2>
-              <h2>{prop.pending}</h2>
+              <h2>Death Total: {prop.death}</h2>
+              <h2>Death Increase: {prop.deathIncrease}</h2>
+              <h2>Hospitilized: {prop.hospitalized}</h2>
+              <h2>Hospitilized Currently: {prop.hospitalizedCurrently}</h2>
+              <h2>Hospitalized Increase: {prop.hospitalizedIncrease}</h2>
+              <h2>Total ICU: {prop.inIcuCumulative}</h2>
+              <h2>Currently in ICU:{prop.inIcuCurrently}</h2>
+              <h2>Negative: {prop.negative}</h2>
+              <h2>Negative Increase: {prop.negativeIncrease}</h2>
+              <h2>Positive: {prop.positive}</h2>
+              <h2>Positive Increase: {prop.positiveIncrease}</h2>
+              <h2>Currently on Ventilator: {prop.onVentilatorCurrently}</h2>
+              <h2>
+                Total on Ventilator: {prop.onVentilatoronVentilatorCumulative}
+              </h2>
+              <h2>Pending: {prop.pending}</h2>
             </div>
           );
         })}
